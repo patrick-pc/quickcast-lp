6e3108130cd55e6718c934c8dd667489bc5a3d2a
spidΞy <web3slinger@gmail.com>
2023-04-28 18:13:01 -0500

    feat: add og image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,20 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
+        <meta property="og:url" content="https://quickcast.lol/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="quickcast" />
+        <meta property="og:description" content="" />
+        <meta property="og:site_name" content="quickcast" />
+        <meta property="og:image" content="https://quickcast.lol/images/og-image.png" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="quickcast" />
+        <meta name="twitter:description" content="" />
+        <meta name="twitter:image" content="https://quickcast.lol/images/og-image.png" />
+
+        <meta name="theme-color" content="##09090B" />
+
         <Main />
         <NextScript />
       </body>
