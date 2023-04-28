de9b3600c895dfa5e56f230f0b1a797c34e4adcc
spidΞy <web3slinger@gmail.com>
2023-04-28 18:38:32 -0500

    patch: add og description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,13 +8,19 @@ export default function Document() {
         <meta property="og:url" content="https://quickcast.lol/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="quickcast" />
-        <meta property="og:description" content="" />
+        <meta
+          property="og:description"
+          content="Quick launch ChatGPT with a single shortcut key."
+        />
         <meta property="og:site_name" content="quickcast" />
         <meta property="og:image" content="https://quickcast.lol/images/og-image.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="quickcast" />
-        <meta name="twitter:description" content="" />
+        <meta
+          name="twitter:description"
+          content="Quick launch ChatGPT with a single shortcut key."
+        />
         <meta name="twitter:image" content="https://quickcast.lol/images/og-image.png" />
 
         <meta name="theme-color" content="##09090B" />
