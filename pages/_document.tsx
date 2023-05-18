cd96ee54560b1b854027bed5de257dc6fc2d4d43
spidΞy <web3slinger@gmail.com>
2023-05-18 09:06:46 -0500

    patch: update og title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,20 +7,14 @@ export default function Document() {
       <body>
         <meta property="og:url" content="https://quickcast.lol/" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="quickcast" />
-        <meta
-          property="og:description"
-          content="Boost your efficiency with hotkeys and AI."
-        />
-        <meta property="og:site_name" content="quickcast" />
+        <meta property="og:title" content="QuickCast" />
+        <meta property="og:description" content="Boost your efficiency with hotkeys and AI." />
+        <meta property="og:site_name" content="QuickCast" />
         <meta property="og:image" content="https://quickcast.lol/images/og-image.png" />
 
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="quickcast" />
-        <meta
-          name="twitter:description"
-          content="Boost your efficiency with hotkeys and AI."
-        />
+        <meta name="twitter:title" content="QuickCast" />
+        <meta name="twitter:description" content="Boost your efficiency with hotkeys and AI." />
         <meta name="twitter:image" content="https://quickcast.lol/images/og-image.png" />
 
         <meta name="theme-color" content="##09090B" />
